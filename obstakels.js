7fce1166a3a7902bf345850958b9c25a7c3d898b
Ege <ps253207@summacollege.nl>
2026-03-23 10:07:26 +0100

    score/highscore/newrecord

--- obstakels.js
+++ obstakels.js
@@ -1,6 +1,20 @@
 const dino = document.getElementById("dino");
 const plant = document.getElementById("plant");
 const gameOverText = document.getElementById("gameOver");
+const scoreText = document.getElementById("score");
+const highScoreText = document.getElementById("highscore");
+const newRecordText = document.getElementById("newRecord");
+
+let score = 0;
+let highScore = localStorage.getItem("highScore") || 0;
+let recordBroken = false;
+
+highScore = Number(highScore);
+
+scoreText.textContent = "Score: 0";
+highScoreText.textContent = "Highscore: " + highScore;
+newRecordText.style.display = "none";
+gameOverText.style.display = "none";
 
 function jump() {
   if (!dino.classList.contains("jump")) {
@@ -8,27 +22,43 @@ function jump() {
 
     setTimeout(function () {
       dino.classList.remove("jump");
-    }, 300   );
+    }, 300);
   }
 }
 
-let isAlive = setInterval(function () {
-  let dinoTop = parseInt(
-    window.getComputedStyle(dino).getPropertyValue("top")
-  );
+let scoreInterval = setInterval(function () {
+  score++;
+  scoreText.textContent = "Score: " + score;
 
-  let plantLeft = parseInt(
-    window.getComputedStyle(plant).getPropertyValue("left")
-  );
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem("highScore", highScore);
+    highScoreText.textContent = "Highscore: " + highScore;
+
+    if (!recordBroken) {
+      newRecordText.style.display = "block";
+      recordBroken = true;
+
+      setTimeout(function () {
+        newRecordText.style.display = "none";
+      }, 2000);
+    }
+  }
+}, 100);
+
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let plantLeft = parseInt(window.getComputedStyle(plant).getPropertyValue("left"));
 
   if (plantLeft > 0 && plantLeft < 50 && dinoTop >= 140) {
     clearInterval(isAlive);
-        plant.style.animation = "none";
-        plant.style.display = "none";
-        dino.style.animation = "none";
-        gameOverText.style.display = "block";
+    clearInterval(scoreInterval);
+
+    plant.style.animation = "none";
+    plant.style.display = "none";
+    gameOverText.style.display = "block";
   }
-}, 10);
+}, 20);
 
 document.addEventListener("keydown", function (event) {
   if (event.code === "Space") {
